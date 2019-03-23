18443db61cc7bf4496c67944001c5804139c7607
Shrinivas Shukla <ssshukla1993@gmail.com>
2019-03-23 19:20:53 +0530

    updated ir watcher interval

--- ir_watcher.py
+++ ir_watcher.py
@@ -9,7 +9,8 @@ IO.setup(IR_PIN_NO, IO.IN)
 
 
 def on_ir_state_changed(previous_state, current_state):
-    print('Handler 1 : {} to {}'.format(previous_state, current_state))
+    if previous_state == 1 and current_state == 0:
+        print('Handler 1 : {} to {}'.format(previous_state, current_state))
 
 
 def get_ir_state(_ir_pin_no):
@@ -17,6 +18,6 @@ def get_ir_state(_ir_pin_no):
 
 
 ir_watcher = Watcher(state_func=get_ir_state, change_handlers=[
-    on_ir_state_changed], initial_state=get_ir_state(IR_PIN_NO), state_check_interval=1, _ir_pin_no=IR_PIN_NO)
+    on_ir_state_changed], initial_state=get_ir_state(IR_PIN_NO), state_check_interval=0.1, _ir_pin_no=IR_PIN_NO)
 
 ir_watcher.start_watching()
