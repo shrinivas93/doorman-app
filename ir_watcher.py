965d13cf86ebd528d45f1a952e75f7fadc954530
Shrinivas Shukla <ssshukla1993@gmail.com>
2019-03-23 21:25:41 +0530

    fixing pylint issues

--- ir_watcher.py
+++ ir_watcher.py
@@ -1,11 +1,11 @@
+from configparser import ConfigParser
 import RPi.GPIO as IO
 from events.watcher import Watcher
-from configparser import ConfigParser
 
-config = ConfigParser()
-config.read('./config/config.ini')
+CONFIG = ConfigParser()
+CONFIG.read('./config/config.ini')
 
-IR_PIN_NO = int(config['DEFAULT']['IR_PIN_NO'])
+IR_PIN_NO = int(CONFIG['DEFAULT']['IR_PIN_NO'])
 
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
@@ -21,7 +21,12 @@ def get_ir_state(_ir_pin_no):
     return IO.input(_ir_pin_no)
 
 
-ir_watcher = Watcher(state_func=get_ir_state, change_handlers=[
-    on_ir_state_changed], initial_state=get_ir_state(IR_PIN_NO), state_check_interval=0.1, _ir_pin_no=IR_PIN_NO)
+IR_WATCHER = Watcher(
+    state_func=get_ir_state,
+    change_handlers=[on_ir_state_changed],
+    initial_state=get_ir_state(IR_PIN_NO),
+    state_check_interval=0.1,
+    _ir_pin_no=IR_PIN_NO
+)
 
-ir_watcher.start_watching()
+IR_WATCHER.start_watching()
